5449d7c78e503d8b0fd9bbd69a5d3c1285e12ede
Eagle1191 <Werise92@gmail.com>
2017-04-23 01:50:40 -0700

    adding procfile

--- CSWeb/wsgi.py
+++ CSWeb/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import os
+from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CSWeb.settings")
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
-application = get_wsgi_application()
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CSWeb.settings")
+
+#application = get_wsgi_application()
